5e1096ef3b2d9cf48dac77d0815ca503fa68434f
Maria Eduarda Braga <maria.e.ferreira77@aluno.senai.br>
2025-09-15 15:59:04 -0300

    feat: cria as queries de select

---
+++ 02-selects.sql
@@ -0,0 +1,15 @@
+SELECT * FROM artistas;
+
+SELECT COUNT (*) AS total_artistas FROM artistas;
+
+SELECT nome, genero_musical FROM artistas;
+
+SELECT nome, genero_musical FROM artistas
+WHERE pais_origem = 'Brasil';
+
+SELECT nome, genero_musical FROM artistas
+WHERE id = 9;
+
+SELECT nome, genero_musical FROM artistas
+WHERE id = 9 OR id = 3;
+
--- queries/01-insert-artists.sql
+++ queries/01-insert-artists.sql
@@ -10,3 +10,19 @@ CREATE TABLE artistas (
     musica_famosa VARCHAR(50),
     ativo BOOLEAN
 );
+
+INSERT INTO artistas (nome, tipo, membros, genero_musical, ano_lancamento, pais_origem, musica_famosa, ativo)
+VALUES
+('Adele', 'Solo', 1, 'Pop', 2006, 'Reino Unido', 'Rolling in the Deep', TRUE),
+('Justin Bieber', 'Solo', 1, 'Pop', 2010, 'Canadá', 'Baby', TRUE),
+('Katy Perry', 'Solo', 1, 'Pop', 2011, 'EUA', 'Last Friday Night', TRUE ),
+('Beyoncé', 'Solo', 1, 'R&B/Pop', 2003, 'EUA', 'Crazy in Love', TRUE),
+('Coldplay', 'Banda', 4, 'Rock Alternativo', 2000, 'Reino Unido', 'Yellow', TRUE),
+('Ed Sheeran', 'Solo', 1, 'Pop/Folk', 2011, 'Reino Unido', 'Shape of You', TRUE),
+('Drake', 'Solo', 1, 'Hip Hop', 2009, 'Canadá', 'Hotline Bling', TRUE),
+('Taylor Swift', 'Solo', 1, 'Pop/Country', 2006, 'EUA', 'Love Story', TRUE),
+('Maroon 5', 'Banda', 6, 'Pop/Rock', 2002, 'EUA', 'Sugar', TRUE),
+('The Weeknd', 'Solo', 1, 'R&B/Pop', 2013, 'Canadá', 'Blinding Lights', TRUE),
+('Imagine Dragons', 'Banda', 4, 'Rock Alternativo', 2012, 'EUA', 'Radioactive', TRUE),
+('Shakira', 'Solo', 1, 'Pop/Latino', 1995, 'Colômbia', 'Hips Don’t Lie', TRUE),
+('Queen', 'Banda', 4, 'Rock', 1973, 'Reino Unido', 'Bohemian Rhapsody', FALSE)
