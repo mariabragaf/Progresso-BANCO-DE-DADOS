a14f63114f334748fdbb3815c9d45a820748594e
Maria Eduarda Braga <maria.e.ferreira77@aluno.senai.br>
2025-09-22 09:18:49 -0300

    feat: cria comandos basicos de delete e contagem final

---
+++ 04-delete.sql
@@ -0,0 +1,17 @@
+DELETE FROM usuarios 
+WHERE username = 'CheatCodeKing';
+
+SELECT * FROM usuarios;
+
+DELETE FROM usuarios
+WHERE id= 13;
+
+SELECT * FROM usuarios;
+
+DELETE FROM usuarios
+WHERE pontos < 2000;
+
+SELECT * FROM usuarios;
+
+SELECT COUNT(*) AS total_usuarios FROM usuarios;
+
