90c016ee5285b07cb2dd3e048e2b946dfe01bbdf
Maria Eduarda Braga <maria.e.ferreira77@aluno.senai.br>
2025-09-22 08:59:56 -0300

    feat: comandos iniciais de update

---
+++ 03-update.sql
@@ -0,0 +1,13 @@
+UPDATE usuarios SET username = 'ProGamer2025'
+WHERE username = 'NoobMaster';
+
+SELECT * FROM usuarios;
+
+UPDATE usuarios SET pontos + 1000;
+
+SELECT * FROM usuarios;
+
+UPDATE usuarios SET email = 'main@gmail.com'
+WHERE email = 'master@email.com';
+
+SELECT * FROM usuarios;
